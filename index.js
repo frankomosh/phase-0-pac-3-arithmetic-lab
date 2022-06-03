2d74da685665acca590284a2dd7718d92cbae07f
Frank-197 <frankomosh197@gmail.com>
2022-06-03 12:24:31 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+   return a+b;
+}
+function subtract(a,b){
+   return a-b;
+}
+function multiply(a,b){
+   return a*b; 
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(n=0){
+    n++;
+    return n;
+}
+function decrement(n=0){
+    n--;
+    return n;
+
+}
+function preserveDecimal(n){
+     n=10.256;
+    return parseFloat(n)
+}
+function makeInt(n,parseInt){
+    pareseInt=10;
+    return n;
+}
+function makeInt(string){
+        return string*1;
+}
+function preserveDecimal(string){
+    return NaN;
+}
